79bb8b95b37487b4681b021cffcccd75f5c5c9b2
Moza <mozaalmamri16@gmail.com>
2026-08-05 09:29:00 +0400

    complete car model

--- CarRentakSystem/CarRentalSystem/Models/Car.cs
+++ CarRentakSystem/CarRentalSystem/Models/Car.cs
@@ -2,6 +2,12 @@
 {
     public class Car
     {
-        
+        public int CarId { get; set; }
+        public string PlateNumber { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int year { get; set; }
+        public decimal DailyRate { get; set; }
+        public bool IsAvailable { get; set; }
     }
 }
