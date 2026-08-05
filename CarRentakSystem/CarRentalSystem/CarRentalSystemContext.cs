d94111b79578a60c298217dbb5d26a89219e2210
Sultan AL Maskari <21f22179>
2026-08-05 20:31:00 +0400

    added models: (Rental and Payments) updated DbContext

--- CarRentakSystem/CarRentalSystem/CarRentalSystemContext.cs
+++ CarRentakSystem/CarRentalSystem/CarRentalSystemContext.cs
@@ -7,6 +7,7 @@ namespace CarRentalSystem
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Car> Car { get; set; }
-
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<Payments> Payments { get; set; }
     }
 }
