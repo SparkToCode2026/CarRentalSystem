c8434e966def24a14a8431faddb9d30ff57bf0bd
Shams Al Ghusaini <shamsalghusainii@gmail.com>
2026-08-05 21:56:33 +0400

    Remove unnecessary Data Annotations

--- CarRentakSystem/CarRentalSystem/Models/Branch.cs
+++ CarRentakSystem/CarRentalSystem/Models/Branch.cs
@@ -1,21 +1,10 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace CarRentalSystem.Models
 {
     public class Branch
     {
         public int Id { get; set; }
-
-        [Required]
-        [MaxLength(100)]
         public string Name { get; set; } = string.Empty;
-
-        [Required]
-        [MaxLength(100)]
         public string City { get; set; } = string.Empty;
-
-        [Required]
-        [MaxLength(200)]
         public string Address { get; set; } = string.Empty;
     }
 }
