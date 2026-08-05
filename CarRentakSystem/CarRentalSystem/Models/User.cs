3b186aeeefeb1eec841500c879cc4837023c5c0c
Moza <mozaalmamri16@gmail.com>
2026-08-05 09:26:21 +0400

    create user and car models

--- CarRentakSystem/CarRentalSystem/Models/User.cs
+++ CarRentakSystem/CarRentalSystem/Models/User.cs
@@ -2,5 +2,18 @@
 {
     public class User
     {
+        public enum UserRole
+        {
+            Admin,
+            Customer,
+            staff
+        }
+        public string userId { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string passwordHash { get; set; }
+        public UserRole role { get; set; }
+        public DateTime CreatedAtUtc { get; set; }
+
     }
 }
