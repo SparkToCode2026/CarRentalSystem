4a96a6aacea4581e508e059c8532b341e7889156
malaksalim <malakalburtmani@gmail.com>
2026-08-05 22:04:42 +0400

    add discount model

---
+++ CarRentakSystem/CarRentalSystem/Models/Discount.cs
@@ -0,0 +1,12 @@
+namespace CarRentalSystem.Models;
+
+public class Discount
+{
+    public int Id { get; set; }
+    
+    public string Code { get; set; }
+    
+    public decimal Percent { get; set; }
+
+    public DateTime ExpiresOn { get; set; }
+}
