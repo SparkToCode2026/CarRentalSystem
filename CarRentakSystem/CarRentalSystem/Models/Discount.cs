675bc3bf403fedebb304b2fe75ce92ba3cbc5f23
malaksalim <malakalburtmani@gmail.com>
2026-08-06 11:35:25 +0400

    last

--- CarRentakSystem/CarRentalSystem/Models/Discount.cs
+++ CarRentakSystem/CarRentalSystem/Models/Discount.cs
@@ -1,12 +1,12 @@
 namespace CarRentalSystem.Models;
 
-public class Discount
-{
-    public int Id { get; set; }
-    
-    public string Code { get; set; }
-    
-    public decimal Percent { get; set; }
+    public class Discount
+    {
+        public int Id { get; set; }
 
-    public DateTime ExpiresOn { get; set; }
-}
+        public string Code { get; set; }
+
+        public decimal Percent { get; set; }
+
+        public DateTime ExpiresOn { get; set; }
+    }
