6b821ae2026268c4998699e745e69e157c064895
Shams Al Ghusaini <shamsalghusainii@gmail.com>
2026-08-06 13:33:27 +0400

    Implement CarCategory and Branch relationships

--- CarRentakSystem/CarRentalSystem/CarRentalSystemContext.cs
+++ CarRentakSystem/CarRentalSystem/CarRentalSystemContext.cs
@@ -1,7 +1,12 @@
-﻿namespace CarRentalSystem
+﻿using CarRentalSystem.Models;
+
+namespace CarRentalSystem
 {
     public class CarRentalSystemContext
     {
-        
+        public DbSet<CarCategory> CarCategories { get; set; }
+
+        public DbSet<Branch> Branches { get; set; }  
     }
 }
+
