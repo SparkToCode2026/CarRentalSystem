4fdc3c5f011aeb0cdfca2cb4e9aaff5026974eb0
Shams Al Ghusaini <shamsalghusainii@gmail.com>
2026-08-06 22:55:30 +0400

    Fix navigation properties placement

--- CarRentakSystem/CarRentalSystem/Models/Branch.cs
+++ CarRentakSystem/CarRentalSystem/Models/Branch.cs
@@ -6,8 +6,9 @@ namespace CarRentalSystem.Models
         public string Name { get; set; } = string.Empty;
         public string City { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
-    }
-    public ICollection<Car> Cars { get; set; } = new List<Car>();
 
-    public ICollection<Rental> PickupRentals { get; set; } = new List<Rental>();
+        public ICollection<Car> Cars { get; set; } = new List<Car>();
+
+        public ICollection<Rental> PickupRentals { get; set; } = new List<Rental>();
+    }
 }
