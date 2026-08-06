41217cc9c471376ac3c776d5de6a7ea9a81722e6
malaksalim <malakalburtmani@gmail.com>
2026-08-06 15:24:36 +0400

    DamageReport

--- CarRentakSystem/CarRentalSystem/Models/DamageReport.cs
+++ CarRentakSystem/CarRentalSystem/Models/DamageReport.cs
@@ -4,9 +4,20 @@ public class DamageReport
 {
     public int DamageReport_ID { get; set; }
     
-    public string Description { get; set; }
+    public string Description { get; set; } = string.Empty
     
     public DateTime ReortedAtUtc { get; set; }
     
     public decimal RepairCost { get; set; }
+    
+    // foreign keys 
+    
+    public int CarId { get; set; }
+    public int RentalId { get; set; }
+
+    public Car Car { get; set; } = null!;
+    public Rental Rental { get; set; } = null!;
+
+
+
 }
