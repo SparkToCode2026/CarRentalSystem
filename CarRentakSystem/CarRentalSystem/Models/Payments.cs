5de1c6fc2cc872eb824efa56642f815354d9817d
Sultan AL Maskari <21f22179>
2026-08-06 20:32:06 +0400

    added relationships of Rental and Payment models to other models

--- CarRentakSystem/CarRentalSystem/Models/Payments.cs
+++ CarRentakSystem/CarRentalSystem/Models/Payments.cs
@@ -8,5 +8,8 @@
         public string Method { get; set; }
         public DateTime PaidAtUtc { get; set; }
         public string Status { get; set; } = string.Empty;
+
+        //FK with Rental model (one-to-one)
+        public IList<Rental> Rentals { get; set; } = new List<Rental>();
     }
 }
--- CarRentakSystem/CarRentalSystem/Models/Rental.cs
+++ CarRentakSystem/CarRentalSystem/Models/Rental.cs
@@ -7,12 +7,24 @@ namespace CarRentalSystem.Models
 {
     public class Rental
     {
-        
         public int Rental_ID { get; set; }
         public DateOnly StartDate { get; set; } //= new DateOnly();
         public DateOnly DueDate { get; set; }
         public DateTime ReturnAtUtc { get; set; }
         public string Status { get; set; } = string.Empty;
         public int TotalDays { get; set; }
+
+        //Foreign Key Relationships
+        public ICollection<Car> Cars { get; set; } = new List<Car>();               //FK: Rental → Car (Many-to-One)
+        public ICollection<User> Users { get; set; } = new List<User>();          //FK: Rental → User (Many-to-One)
+        public ICollection<Branch> Branches { get; set; } = new List<Branch>();  //FK: Rental → Branch (Many-to-One)
+        public ICollection<Payments> Payments { get; set; } = new List<Payments>();               //FK: Rental → Payments (One-to-One)
+        //public ICollection<Insurance> Insurances { get; set; } = new List<Insurance>();        //FK: Rental → Insurance (One-to-Many)  
+        //public ICollection<DamageReport> DamageReports { get; set; } = new List<DamageReport>();              //FK: Rental → DamageReport (One-to-Many)
+        //Public ICollection<RentalDiscount> RentalDiscounts { get; set; } = new List<RentalDiscount>();       //FK: Rental → RentalDiscount (One-to-Many)
+        //public IList<DriverProfile> DriverProfiles { get; set; } = new List<DriverProfile>();               // FK: Rental → DriverProfile (Many-to-One)
+        
     }
 }
+//============IMPORTANT NOTE:==========
+//needs to be updated later since this branch dosent have the insurance, damage report, rental discount, and driver profile models yet.
