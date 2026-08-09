4bf7867ca07fed62777ef6ce761fbb220a0bdb4c
Shams Al Ghusaini <shamsalghusainii@gmail.com>
2026-08-09 09:07:41 +0400

    Add Branch GET endpoints

--- CarRentakSystem/CarRentalSystem/Controllers/BranchController.cs
+++ CarRentakSystem/CarRentalSystem/Controllers/BranchController.cs
@@ -1,6 +1,45 @@
-﻿namespace CarRentalSystem.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRentalSystem.Models;
+
+namespace CarRentalSystem.Controllers
 {
-    public class BranchController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BranchController : ControllerBase
     {
+        private readonly CarRentalSystemContext _context;
+
+        public BranchController(CarRentalSystemContext context)
+        {
+            _context = context;
+        }
+        
+        // GET: api/Branch
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Branch>>> GetBranches()
+        {
+            return await _context.Branches
+                .Include(b => b.Cars)
+                .Include(b => b.PickupRentals)
+                .ToListAsync();
+        }
+
+        // GET: api/Branch/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Branch>> GetBranch(int id)
+        {
+            var branch = await _context.Branches
+                .Include(b => b.Cars)
+                .Include(b => b.PickupRentals)
+                .FirstOrDefaultAsync(b => b.Id == id);
+
+            if (branch == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(branch);
+        }
     }
 }
