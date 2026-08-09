baabc88d8813cac73ab7cc8dd1db1387059d9bc1
Shams Al Ghusaini <shamsalghusainii@gmail.com>
2026-08-09 09:09:17 +0400

    Add Branch create and update endpoints

--- CarRentakSystem/CarRentalSystem/Controllers/BranchController.cs
+++ CarRentakSystem/CarRentalSystem/Controllers/BranchController.cs
@@ -41,5 +41,66 @@ namespace CarRentalSystem.Controllers
 
             return Ok(branch);
         }
+        // POST: api/Branch
+        [HttpPost]
+        public async Task<ActionResult<Branch>> CreateBranch(Branch branch)
+        {
+            _context.Branches.Add(branch);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetBranch),
+                new { id = branch.Id },
+                branch);
+        }
+
+// PUT: api/Branch/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBranch(
+            int id,
+            Branch branch)
+        {
+            if (id != branch.Id)
+            {
+                return BadRequest("ID does not match.");
+            }
+
+            var existingBranch = await _context.Branches
+                .FindAsync(id);
+
+            if (existingBranch == null)
+            {
+                return NotFound();
+            }
+
+            existingBranch.Name = branch.Name;
+            existingBranch.City = branch.City;
+            existingBranch.Address = branch.Address;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // PUT: api/Branch/5/city
+        [HttpPut("{id}/city")]
+        public async Task<IActionResult> UpdateCity(
+            int id,
+            string city)
+        {
+            var branch = await _context.Branches
+                .FindAsync(id);
+
+            if (branch == null)
+            {
+                return NotFound();
+            }
+
+            branch.City = city;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
