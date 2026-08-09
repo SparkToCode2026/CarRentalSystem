6eea6d45e36f33eccd35590cad4921efa42f6100
Moza <mozaalmamri16@gmail.com>
2026-08-09 12:21:52 +0400

    Implement GET ALL.

--- CarRentakSystem/CarRentalSystem/Controllers/CarController.cs
+++ CarRentakSystem/CarRentalSystem/Controllers/CarController.cs
@@ -1,6 +1,92 @@
-﻿namespace CarRentalSystem.Controllers
+﻿using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
+using CarRentalSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace CarRentalSystem.Controllers
 {
-    public class CarController
+    [ApiController]
+    [Route("Car")]
+    public class CarController : ControllerBase
     {
+
+        private CarRentalSystemContext context;
+        public CarController(CarRentalSystemContext _context)
+        {
+            context = _context;
+        }
+
+        //Implement POST to create a new record
+        [HttpPost("AddCar")]
+        public IActionResult AddCar(Car car)
+        {
+            context.Cars.Add(car);
+            context.SaveChanges();
+            return Ok(car.CarId);
+        }
+
+        //PUT to update an existing record
+        [HttpPut("UpdateCar")]
+        public IActionResult UpdateCar(int id, Car updatedCar)
+        {
+            Car ? car = context.Cars.FirstOrDefault(car => car.CarId == id);
+            if (car == null)
+            {
+                return NotFound("car not found");
+            }
+            car.PlateNumber = updatedCar.PlateNumber;
+            car.Make = updatedCar.Make;
+            car.Model = updatedCar.Model;
+            car.year = updatedCar.year;
+            car.DailyRate = updatedCar.DailyRate;
+            car.IsAvailable = updatedCar.IsAvailable;
+            car.CarCategoryId = updatedCar.CarCategoryId;
+            car.BranchId = updatedCar.BranchId;
+            context.SaveChanges();
+            return Ok("updated successfully");
+        }
+        //PATCH endpoint for Availability
+        [HttpPatch("UpdateCarAvailability")]
+        public IActionResult UpdateCarAvailability(int id, bool isAvailable)
+        {
+            Car
+                ? car = context.Cars.FirstOrDefault(c => c.CarId == id);
+
+            if (car == null)
+            {
+                return NotFound("car not found");
+            }
+
+            car.IsAvailable = isAvailable;
+
+            context.SaveChanges();
+
+            return Ok("car availability updated successfully");
+        }
+
+        // DELETE by ID
+        [HttpDelete("RemoveCar")]
+        public IActionResult RemoveCar(int id)
+        {
+            Car? car = context.Cars.FirstOrDefault(c => c.CarId == id);
+
+            if (car == null)
+            {
+                return NotFound("car not found");
+            }
+
+            context.Cars.Remove(car);
+            context.SaveChanges();
+
+            return Ok("car removed successfully");
+        }
+
+        // GET ALL
+        [HttpGet("GetAllCars")]
+        public IActionResult GetAllCars()
+        {
+            List<Car> cars = context.Cars.ToList();
+
+            return Ok(cars);
+        }
     }
 }
--- CarRentakSystem/CarRentalSystem/Models/Car.cs
+++ CarRentakSystem/CarRentalSystem/Models/Car.cs
@@ -1,39 +1,55 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarRentalSystem.Models
 {
     public class Car
     {
+        [Key]
+        [JsonIgnore]
         public int CarId { get; set; }
+        
         public string PlateNumber { get; set; }
+        
         public string Make { get; set; }
+        
         public string Model { get; set; }
+        
         public int year { get; set; }
+        
         [Precision(10, 2)]
         public decimal DailyRate { get; set; }
+        
         public bool IsAvailable { get; set; }
 
 
         // One-to-many: a Car is rented out across many Rentals
+        [JsonIgnore]
         public ICollection<Rental> Rentals { get; set; } = new List<Rental>();
         // Many-to-one: many Cars belong to one CarCategory
         [ForeignKey("CarCategory")]
-        public int CarCategoryId { get; set; } 
-        public CarCategory CarCategory { get; set; }  // Navigation property 
+        public int CarCategoryId { get; set; }
+        [JsonIgnore]
+        public CarCategory ? CarCategory { get; set; }  // Navigation property 
 
         // Many-to-one: many Cars are stationed at one Branch
         [ForeignKey("Branch")]
         public int BranchId { get; set; }
-        public Branch Branch { get; set; }
+        [JsonIgnore]
+        public Branch ? Branch { get; set; }
 
         // One-to-many: a Car is serviced across many Maintenance records
+        [JsonIgnore]
         public ICollection<Maintenance> Maintenances { get; set; } = new List<Maintenance>();
 
         // One-to-many: a Car can have many DamageReports
+        [JsonIgnore]
         public ICollection<DamageReport> DamageReports { get; set; } = new List<DamageReport>();
 
         // One-to-many: a Car can have many reviews
+        [JsonIgnore]
         public ICollection<Review> Reviews { get; set; } = new List<Review>();
 
     }
