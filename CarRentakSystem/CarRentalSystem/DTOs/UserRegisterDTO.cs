1f4d8e980312bb8de8475e392cac103580e7bb25
Sultan AL Maskari <21f22179>
2026-08-10 19:42:34 +0400

    added 2 DTOs for data transfer between Swagger and API

---
+++ CarRentakSystem/CarRentalSystem/DTOs/UserRegisterDTO.cs
@@ -0,0 +1,12 @@
+﻿using static CarRentalSystem.Models.User;
+
+namespace CarRentalSystem.DTOs
+{
+    public class UserRegisterDto
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; } // password input by user
+        public UserRole role { get; set; }
+    }
+}
