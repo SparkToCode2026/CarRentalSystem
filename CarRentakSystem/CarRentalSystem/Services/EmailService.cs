d84938a379ca3b3b533052fb3405a8263d313887
Shams Al Ghusaini <shamsalghusainii@gmail.com>
2026-08-10 20:33:01 +0400

    Implement email service with MailKit

---
+++ CarRentakSystem/CarRentalSystem/Services/EmailService.cs
@@ -0,0 +1,55 @@
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+
+namespace CarRentalSystem.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailSettings _settings;
+
+        public EmailService(IOptions<EmailSettings> options)
+        {
+            _settings = options.Value;
+        }
+
+        public async Task SendEmailAsync(
+            string toEmail,
+            string subject,
+            string htmlBody)
+        {
+            var message = new MimeMessage();
+
+            message.From.Add(
+                new MailboxAddress(
+                    _settings.SenderName,
+                    _settings.SenderEmail));
+
+            message.To.Add(
+                MailboxAddress.Parse(toEmail));
+
+            message.Subject = subject;
+
+            message.Body = new BodyBuilder
+            {
+                HtmlBody = htmlBody
+            }.ToMessageBody();
+
+            using var client = new SmtpClient();
+
+            await client.ConnectAsync(
+                _settings.SmtpServer,
+                _settings.SmtpPort,
+                SecureSocketOptions.StartTls);
+
+            await client.AuthenticateAsync(
+                _settings.Username,
+                _settings.AppPassword);
+
+            await client.SendAsync(message);
+
+            await client.DisconnectAsync(true);
+        }
+    }
+}
