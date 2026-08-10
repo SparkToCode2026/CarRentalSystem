2942ef44aabfef7184295054fbfb1291950a24e1
Shams Al Ghusaini <shamsalghusainii@gmail.com>
2026-08-10 20:38:33 +0400

    Register email service in dependency injection

--- CarRentakSystem/CarRentalSystem/Program.cs
+++ CarRentakSystem/CarRentalSystem/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using CarRentalSystem.Services;
 namespace CarRentalSystem
+
 {
     public class Program
     {
@@ -11,7 +13,11 @@ namespace CarRentalSystem
             builder.Services.AddDbContext<CarRentalSystemContext>(options =>
                options.UseSqlServer(
                    builder.Configuration.GetConnectionString("DefaultConnection")));
+            
+            builder.Services.Configure<EmailSettings>(
+                builder.Configuration.GetSection("EmailSettings"));
 
+            builder.Services.AddScoped<IEmailService, EmailService>();
 
             builder.Services.AddControllers();
             //////////////////////////////////////////////////////
