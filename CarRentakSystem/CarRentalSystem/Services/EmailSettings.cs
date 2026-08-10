b041313a86d7fcabad08f3e57e2cedd9f28c4778
Shams Al Ghusaini <shamsalghusainii@gmail.com>
2026-08-10 20:18:38 +0400

    add email settings model

---
+++ CarRentakSystem/CarRentalSystem/Services/EmailSettings.cs
@@ -0,0 +1,12 @@
+namespace CarRentalSystem.Services
+{
+    public class EmailSettings
+    {
+        public string SmtpServer { get; set; } = string.Empty;
+        public int SmtpPort { get; set; }
+        public string SenderName { get; set; } = string.Empty;
+        public string SenderEmail { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string AppPassword { get; set; } = string.Empty;
+    }
+}
